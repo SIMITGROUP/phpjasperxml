358d0ee36845ba9042836920e41b6547a7283233
Ks Tan <kstan@simitgroup.com>
2018-10-13 14:15:51 +0800

    improve list

--- sample/index.php
+++ sample/index.php
@@ -1,25 +1,37 @@
 <?php
 include "setting.php";
-echo <<< EOF
+echo <<< HTML
 
 <html>
-<image src="simitlogo.jpg"><br>
-Developer: Ng Jey Ruey (jeyruey@gmail.com)<br>
-Project Leader: KS Tan (kstan@simit.com.my)<br>
-Organization: <a href='http://www.simit.com.my'>Sim IT Sdn Bhd</a><br>
-    <h1>PHP Jasper XML ($version) Example</h1><br>
+<head>
+    <style>
+        li {
+           list-style:none; 
+        }
+    </style>
+</head>
+<body>
+<div style='text-align: center'>
+<h1>PHP Jasper XML ($version) Example</h1><br>
+<image src="simitlogo.png">
+    
 	
     <p><B>Example:</B></p>
-    <ul>
-    <li><a href='sample1.php' target='_blank'>Sample 1 <a> (Standard Parent and Child Report)</li>
-    <li><a href='sample2.php' target='_blank'>Sample 2</a> Charts (pie chart not supported)</li>
-    <li><a href='sample3.php' target='_blank'>Sample 3</a> Sub Reports</li>
-    <li><a href='sample4.php' target='_blank'>Sample 4</a>Export as Excel</li>
-    <li><a href='sample5.php?id=1' target='_blank'>Sample 5</a> (Use TCPDF, with writeHTML output) (markup=html)</li>
-    <li><a href='sample6.php' target='_blank'>Sample 6</a> Grouping, hide repeated value (UOM), with asian fonts</li>
-    <li><a href='sample7.php' target='_blank'>Sample 7</a> Complex Layout</li>
-    </ul>
-<br/>* Thanks rachman sponsored for new function!
+    <div >
+        <ul>
+            <li><a href='sample1.php' target='_blank'>Sample 1 <a> (Standard Parent and Child Report)</li>
+            <li><a href='sample2.php' target='_blank'>Sample 2</a> Charts (pie chart not supported)</li>
+            <li><a href='sample3.php' target='_blank'>Sample 3</a> Sub Reports</li>
+            <li><a href='sample4.php' target='_blank'>Sample 4</a>Export as Excel</li>
+            <li><a href='sample5.php?id=1' target='_blank'>Sample 5</a> (Use TCPDF, with writeHTML output) (markup=html)</li>
+            <li><a href='sample6.php' target='_blank'>Sample 6</a> Grouping, hide repeated value (UOM), with asian fonts</li>
+            <li><a href='sample7.php' target='_blank'>Sample 7</a> Complex Layout</li>
+        </ul>
+    </div>
+    Organization: <a href='http://www.simitgroup.com'>Sim IT Sdn Bhd</a>
+</div>
+
+</body>
 </html>
-EOF;
+HTML;
 ?>
