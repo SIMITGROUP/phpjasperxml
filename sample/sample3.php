a2983a946171e0fcc35ce54a1244f6b2a27f3674
Ks Tan <kstan@simitgroup.com>
2019-05-16 10:16:56 +0800

    fix cannot draw subreport

--- sample/sample3.php
+++ sample/sample3.php
@@ -6,18 +6,10 @@
 
 include_once("../PHPJasperXML.inc.php");
 include_once ('setting.php');
-
-
-
+$phpjasperversion='1.1';
 
 $PHPJasperXML = new PHPJasperXML();
-$PHPJasperXML->debugsql=false;
 $PHPJasperXML->arrayParameter=array("parameter1"=>1);
-$xml =  simplexml_load_file("sample3.jrxml");
-$PHPJasperXML->load_xml_string($xml); //load xml string instead of file
-
+$PHPJasperXML->load_xml_file("sample3.jrxml");
 $PHPJasperXML->transferDBtoArray($server,$user,$pass,$db);
 $PHPJasperXML->outpage("I");    //page output method I:standard output  D:Download file
-
-
-?>
